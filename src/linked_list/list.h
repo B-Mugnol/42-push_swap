13bc8e46848e855f2ac45eb614218fbcfe2787bf
B-Mugnol <bruno_mugnol@hotmail.com>
2022-04-16 20:53:39 +0200

    now has a find function that returns the first node for which compare_fn returns 'true'

--- src/linked_list/list.h
+++ src/linked_list/list.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 19:26:46 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/04/14 21:14:26 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/04/16 20:51:15 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void		append_list_to_node(t_int_list **lst, t_int_list *new);
 void		append_node_to_list(t_int_list **lst, t_int_list *new);
 
 // list_get.c
+t_int_list	*find(t_int_list *lst, int (*compare_fn)(int, int), int test_param);
 t_int_list	*get_last_node(t_int_list *lst);
 int			get_list_size(t_int_list *lst);
 
--- src/linked_list/list_get.c
+++ src/linked_list/list_get.c
@@ -6,11 +6,24 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 20:08:24 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/04/14 20:40:21 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/04/16 20:51:22 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
+
+t_int_list	*find(t_int_list *lst, int (*compare_fn)(int, int), int test_param)
+{
+	if (!lst)
+		return (lst);
+	while (lst)
+	{
+		if (test_function(lst->content, test_param))
+			return (lst);
+		lst = lst->next;
+	}
+	return (NULL);
+}
 
 t_int_list	*get_last_node(t_int_list *lst)
 {
