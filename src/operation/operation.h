1b3670347d407a4cff6f51d8d1fa48a13f238e66
B-Mugnol <bruno_mugnol@hotmail.com>
2022-04-16 19:18:25 +0200

    sa, sb and ss operations

--- src/operation/operation.h
+++ src/operation/operation.h
@@ -6,13 +6,19 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 20:40:18 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/04/06 20:51:13 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/04/16 19:17:52 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OPERATION_H
 # define OPERATION_H
 
-char *swap_test(void);
+#include "list.h"
+#include "ft_printf.h"
+
+// swap.c
+void	swap_a(t_int_list **stack_a);
+void	swap_b(t_int_list **stack_b);
+void	swap_both(t_int_list **stack_a, t_int_list **stack_b);
 
 #endif
--- src/operation/swap.c
+++ src/operation/swap.c
@@ -6,11 +6,41 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 20:44:12 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/04/06 20:53:42 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/04/16 19:17:20 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *swap_test(void)
+#include "operation.h"
+
+static void	swap_first_nodes(t_int_list **lst);
+
+void	swap_a(t_int_list **stack_a)
 {
-	return ("bob\n");
+	swap_first_nodes(stack_a);
+	ft_printf("sa\n");
+}
+
+void	swap_b(t_int_list **stack_b)
+{
+	swap_first_nodes(stack_b);
+	ft_printf("sb\n");
+}
+
+void	swap_both(t_int_list **stack_a, t_int_list **stack_b)
+{
+	swap_first_nodes(stack_a);
+	swap_first_nodes(stack_b);
+	ft_printf("ss\n");
+}
+
+static void	swap_first_nodes(t_int_list **lst)
+{
+	t_int_list	*new_start;
+	t_int_list	*acc;
+
+	new_start = (*lst)->next;
+	acc = new_start->next;
+	new_start->next = *lst;
+	(*lst)->next = acc;
+	*lst = new_start;
 }
