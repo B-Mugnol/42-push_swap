3257073056d5cea9f67c5e61aab4b03b3846281a
B-Mugnol <bruno_mugnol@hotmail.com>
2022-04-16 21:44:03 +0200

    added rotate functions

--- src/operation/operation.h
+++ src/operation/operation.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 20:40:18 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/04/16 20:27:49 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/04/16 21:42:10 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 
 # include "list.h"		// t_int_list and its related functions
 # include "struct.h"	// t_stack_pair declaration
-# include "ft_printf.h"
+# include "ft_printf.h"	// ft_printf function
 
 // swap.c
 void	swap_a(t_stack_pair *stacks);
 void	swap_b(t_stack_pair *stacks);
-void	swap_both(t_stack_pair *stacks);
+void	swap_all(t_stack_pair *stacks);
 
 // push.c
 void	push_to_a(t_stack_pair *stacks);
 void	push_to_b(t_stack_pair *stacks);
+
+// rotate.c
+void	rotate_a(t_stack_pair *stacks);
+void	rotate_b(t_stack_pair *stacks);
+void	rotate_all(t_stack_pair *stacks);
 
 #endif
---
+++ src/operation/rotate.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/16 21:13:46 by bmugnol-          #+#    #+#             */
+/*   Updated: 2022/04/16 21:42:27 by bmugnol-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "operation.h"
+
+static void	rotate(t_int_list **lst);
+
+void	rotate_a(t_stack_pair *stacks)
+{
+	rotate(stacks->stack_a);
+	ft_printf("ra\n");
+}
+
+void	rotate_b(t_stack_pair *stacks)
+{
+	rotate(stacks->stack_b);
+	ft_printf("rb\n");
+}
+
+void	rotate_all(t_stack_pair *stacks)
+{
+	rotate(stacks->stack_a);
+	rotate(stacks->stack_b);
+	ft_printf("rr\n");
+}
+
+static void	rotate(t_int_list **lst)
+{
+	t_int_list *acc;
+
+	if (!lst || !*lst)
+		return ;
+	acc = *lst;
+	*lst = (*lst)->next;
+	acc->next = NULL;
+	append_node_to_list(lst, acc);
+}
