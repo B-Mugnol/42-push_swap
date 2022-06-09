8011526b1797cbb88265f4de0d1d5abd3f2c57e1
B-Mugnol <bruno_mugnol@hotmail.com>
2022-06-09 05:11:30 +0200

    removed auxiliary print functions

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,18 +6,15 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 20:40:16 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/06/02 20:39:37 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/06/09 05:04:41 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	print_list(t_int_list *lst);//
-static void	print_stacks(t_stack_pair stacks);//
 t_int_list	*to_linked_list(int argc, char **argv);
 static int	is_digit_str(const char *str);
 static int	is_equal(int a, int b);
-# include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -37,15 +34,6 @@ int	main(int argc, char **argv)
 	}
 	stacks = (t_stack_pair){.stack_a = &stack_a, .stack_b = &stack_b};
 	exit_status = algorithm_handler(&stacks, argc - 1);
-	// printf("%i, %i\n", stacks.node_count, get_list_size(*stacks.stack_a));
-	// print_stacks(stacks);
-		// reverse_rotate_all(&stacks);
-		// rotate_a(&stacks);
-		// rotate_b(&stacks);
-		// swap_both(&stacks);
-		// push_to_a(&stacks);
-		// push_to_b(&stacks);
-	// print_stacks(stacks);
 	list_clear(stacks.stack_a, NULL);
 	list_clear(stacks.stack_b, NULL);
 	return (exit_status);
@@ -77,35 +65,6 @@ t_int_list	*to_linked_list(int argc, char **argv)
 		curr_node = prev_node;
 	}
 	return (curr_node);
-}
-
-static void	print_list(t_int_list *lst)
-{
-	if (!lst)
-	{
-		printf("Empty list\n");
-		return ;
-	}
-	while (lst)
-	{
-		printf("%i > ", lst->content);
-		lst = lst->next;
-	}
-	printf("\n");
-}
-
-static void	print_stacks(t_stack_pair stacks)
-{
-	printf("a: ");
-	if (!stacks.stack_a)
-		print_list(NULL);
-	else
-		print_list(*stacks.stack_a);
-	printf("b: ");
-	if (!stacks.stack_b)
-		print_list(NULL);
-	else
-		print_list(*stacks.stack_b);
 }
 
 static int	is_equal(int a, int b)
